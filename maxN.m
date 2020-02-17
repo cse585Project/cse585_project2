d13db6232d46b4c94f032e165a8098f9bc682fbe
Yuchen Liu <liu2529508462@gmail.com>
2020-02-17 15:05:21 -0500

    fix find largest n

--- maxN.m
+++ maxN.m
@@ -1,18 +1,14 @@
-function N = S(X)
+function N = maxN(X)
 % X m*n sub image
 % N max n such that X <Minkowski Subtraction> kB ~= empty
 % B=ones(3,3)
 
-B = ones(3,3);
-XB = opening(X,B);
-S = X & ~XB;
+S = X;
 n = 0;
 while any(S(:)) % exists 1
     n = n+1;
     nB = ones(2*n+1,2*n+1);
-    sub = erosion(X,nB);
-    open = opening(sub,B);
-    S = sub & ~open;
+    S = erosion(X,nB);
 end
 N = n-1;
 
