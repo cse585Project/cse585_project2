e23f4b69e393b9d320743d9afe5f873823530a65
Yuhui Lyu <yuhuilu@Yuhuis-MacBook-Pro.local>
2020-02-17 17:11:20 -0500

    little changes

--- pecstrum.m
+++ pecstrum.m
@@ -1,12 +1,12 @@
 function f = pecstrum(U)
 %pecstrum this function calculate the Pattern Spectrum (Pecstrum) of 
 %each sub-image 
-%   input U is a 4x11 matrix which represents the size distribution of
+%   input U is a matrix which represents the size distribution of
 %   each sub-image
-%   output f is a 4x11 matrix which represents the Pecstrum of each 
+%   output f is a matrix which represents the Pecstrum of each 
 %   sub-image
 
-% initialize a 4x11 matrix for return 
+% initialize a matrix for return based on the size of U
 f = zeros(size(U, 1), size(U, 2));
 A = [0; 0; 0; 0];
 % append a zero column at the end of the U matrix, now its size is 4x12
