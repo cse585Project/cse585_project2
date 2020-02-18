8a5ea2ab42cb3930a578f3388ef8083fd6abc2eb
Yuchen Liu <liu2529508462@gmail.com>
2020-02-17 20:55:08 -0500

    comment plot sub images

--- U.m
+++ U.m
@@ -7,7 +7,7 @@ for i=0:r
     nB = ones(2*i+1, 2*i+1);
     tmp = opening(img, nB);
     area(i+1) = sum(tmp(:));
-    subplot(2,6,i+1);imshow(tmp,[]),title('XrB');
+    % subplot(2,7,i+1);imshow(tmp,[]),title('XrB');
     
 end
 
