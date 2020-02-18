4e2c33dff50bd6dfb64229861bf9a649aabaf613
Yuchen Liu <liu2529508462@gmail.com>
2020-02-17 20:54:06 -0500

    change log base to e; fix bug of advanced 0s

--- complexity.m
+++ complexity.m
@@ -4,8 +4,10 @@ num = size(f,1);
 H = zeros(num,1);
 for i=1:num
     j=1;
-    while j <= size(f,2) && f(i,j) ~= 0
-        H(i) = H(i) - f(i,j)*log2(f(i,j));
+    while j <= size(f,2)
+        if f(i,j) ~= 0
+            H(i) = H(i) - f(i,j)*log(f(i,j));
+        end
         j = j+1;
     end
     
